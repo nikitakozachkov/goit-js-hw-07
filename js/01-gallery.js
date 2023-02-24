61626e32e965542097633cfff9289715e92f8d14
Nikita Kozachkov <kozachkovngoat@gmail.com>
2023-02-24 13:18:44 +0200

    upload task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsRef = galleryItemsMarkup(galleryItems);
+
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function galleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img 
+          class="gallery__image" 
+          src="${preview}" 
+          data-source="${original}"
+          alt="${description}" 
+        />
+      </a>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryImg = event.target.classList.contains("gallery__image");
+  const dataSource = event.target.dataset.source;
+
+  if (!isGalleryImg) {
+    return;
+  }
+
+  modalRef(dataSource);
+}
+
+function modalRef(link) {
+  basicLightbox
+    .create(` <img src="${link}"> `, {
+      onShow: (instance) => {
+        window.addEventListener("keydown", function (event) {
+          if (event.code === "Escape") {
+            instance.close();
+          }
+        });
+      },
+    })
+    .show();
+}
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsRef);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsRef = galleryItemMarkup(galleryItems);
+
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function galleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  new SimpleLightbox(".gallery a", {
+    showCounter: false,
+    doubleTapZoom: false,
+    scrollZoom: false,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsRef);
 
 console.log(galleryItems);
